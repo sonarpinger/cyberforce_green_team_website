d0c651f54bf358500b2ca18cc6d4510e66b815fa
sonarping <brandonramirez@nevada.unr.edu>
2024-10-30 11:37:40 -0700

    setting up basic db

--- main.py
+++ main.py
@@ -22,5 +22,29 @@ async def login(request: Request):
 async def root(request: Request):
   return templates.TemplateResponse('index.html', {'request': request})
 
+@app.get("/about")
+async def root(request: Request):
+  return templates.TemplateResponse('about.html', {'request': request})
+
+@app.get("/contact")
+async def root(request: Request):
+  return templates.TemplateResponse('contact.html', {'request': request})
+
+@app.post("/contact")
+async def root(request: Request):
+  return None
+
+@app.post("/login")
+async def login(request: Request):
+  form = await request.form()
+  username = form['username']
+  password = form['password']
+  if username == 'bingus' and password == 'bingus':
+    request.session['user'] = username
+    return RedirectResponse(url='/', status_code=303)
+  else:
+    return RedirectResponse(url='/login', status_code=303)
+
 if __name__ == '__main__':
   print("hi bingus!")
+  uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=True)
